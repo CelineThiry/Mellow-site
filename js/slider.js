d3d43e079d244d2dc3d60223495854f479024dc2
Céline Thiry <thiry.celi@gmail.com>
2016-04-26 16:21:40 +0200

    Header layout nearly done !

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,6 @@ var slide = null;
 $(function() {
 
     var ul = $(".slider ul");
-    var ul_texttop = $(".slider-text-top ul");
     var ul_textbottom = $(".slider-text-bottom ul");
 
     var slide_count = ul.children().length;
@@ -15,16 +14,8 @@ $(function() {
     var first_slide = ul.find("li:first-child");
     var last_slide = ul.find("li:last-child");
 
-    var first_slide_texttop = ul_texttop.find("li:first-child");
-    var last_slide_texttop = ul_texttop.find("li:last-child");
-
-    var first_slide_textbottom = ul_textbottom.find("li:first-child");
-    var last_slide_textbottom = ul_textbottom.find("li:last-child");
-
     // Clone the last slide and add as first li element :)
     last_slide.clone().prependTo(ul);
-    last_slide_texttop.clone().prependTo(ul_texttop);
-    last_slide_textbottom.clone().prependTo(ul_textbottom);
 
     //Ajout des bullets en dessous du slider
     var bullets_html = '<ul>';
@@ -38,34 +29,10 @@ $(function() {
 
     // Clone the first slide and add as last li element
     first_slide.clone().appendTo(ul);
-    first_slide_texttop.clone().appendTo(ul_texttop);
-    first_slide_textbottom.clone().appendTo(ul_textbottom);
 
     ul.css("margin-left", "-100%");
-    ul_texttop.css("margin-left", "-100%");
-    ul_textbottom.css("margin-left", "-100%");
 
     ul.find("li").each(function(indx) {
-        var left_percent = (slide_width_pc * indx) + "%";
-        $(this).css({
-            "left": left_percent
-        });
-        $(this).css({
-            width: (100 / slide_count) + "%"
-        });
-    });
-
-   ul_texttop.find("li").each(function(indx) {
-        var left_percent = (slide_width_pc * indx) + "%";
-        $(this).css({
-            "left": left_percent
-        });
-        $(this).css({
-            width: (100 / slide_count) + "%"
-        });
-    });
-
-   ul_textbottom.find("li").each(function(indx) {
         var left_percent = (slide_width_pc * indx) + "%";
         $(this).css({
             "left": left_percent
@@ -101,16 +68,12 @@ $(function() {
                         // If new slide is before first slide...
             if (new_slide_index < 0) {
                 ul.css("margin-left", ((slide_count) * (-100)) + "%");
-                ul_texttop.css("margin-left", ((slide_count) * (-100)) + "%");
-                ul_textbottom.css("margin-left", ((slide_count) * (-100)) + "%");
                 new_slide_index = slide_count - 1;
                 console.log("main arriere");
             }
             // If new slide is after last slide...
             else if (new_slide_index >= slide_count) {
                 ul.css("margin-left", "-100%");
-                ul_texttop.css("margin-left", "-100%");
-                ul_textbottom.css("margin-left", "-100%");
                 new_slide_index = 0;
                 console.log("main avant");
             }
@@ -130,24 +93,6 @@ $(function() {
 
 
         });
-
-      ul_texttop.animate({
-            "margin-left": margin_left_pc
-        }, 400, function() {
-          anim_is_done = anim_is_done + 1;
-
-
-        });
-
-            ul_textbottom.animate({
-            "margin-left": margin_left_pc
-        }, 400, function() {
-
-          anim_is_done = anim_is_done + 1;
-
-
-        });
-
 
     }
 /*
